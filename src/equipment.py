fix dps target stat calculation

src/equipment.py
@@ -26,6 +26,7 @@ class Equipment:
         self.material = armor_material
         self.index = 0
         self.owner = ""
+        self.is_equipped = False
         self.dir_id = -1
         self.remaining_upgrades = 0
         self.remaining_upgrades_res = 0
@@ -46,15 +47,16 @@ class Equipment:
         self.remaining_upgrades = self.max_upgrades - self.level
         self.remaining_upgrades_res = self.remaining_upgrades
         resistances = list(self.stat_dict.values())[:4]
-        res_target = max(0, 16 - self.quality) + 29
+        res_target = 40 / QUALITY_PROPERTIES[self.quality][0]
         # assume pre upgraded items are good
-        if not (min(resistances) > 20 and self.level > 100):
+        if not (min(resistances) > 20 and self.level > 100 and self.is_equipped):
             # simulate capping resistances
             for res in resistances:
                 # missing resistance -> useless
                 if res == 0:
                     self.remaining_upgrades_res = -1
-                while res < res_target:
+                    return
+                while res <= res_target:
                     if res == 20 or res == 21:
                         res_offset = 3
                     elif 14 <= res < 20 or res == -1:
@@ -66,6 +68,7 @@ class Equipment:
                     # cannot max resistances -> useless
                     if self.remaining_upgrades_res == 0 and min(resistances) < res_target:
                         self.remaining_upgrades_res = -1
+                        return
 
     def get_property_list(self, dirs=None, include_res=False):
         """
@@ -115,6 +118,25 @@ class Equipment:
         return sum((UPGRADE_COST_BASE * level ** UPGRADE_COST_EXP for level in range(self.level,
                                                                                      self.max_upgrades)))
 
+    def get_boosted_stats(self, effective_stats):
+        """
+        Calculates effective stats after armor set boni.
+
+        Parameters:
+            effective_stats (np.array): Contains current stats of this item
+
+        Returns:
+            np.array: The boosted stats
+        """
+        boosted_stats = np.array(copy.deepcopy(effective_stats))
+        if self.type != "Armor":
+            return boosted_stats
+        quality_mult = QUALITY_PROPERTIES[self.quality][0]
+        boosted_stats_mask = boosted_stats > 0
+        boosted_stats_mask = boosted_stats_mask * quality_mult + ~boosted_stats_mask
+        boosted_stats = boosted_stats * boosted_stats_mask
+        return np.ceil(boosted_stats).astype(int)
+
     def get_weighted_score(self, weights, upgrade_accs=True):
         """
         Simulates upgrading the item to max rank, stats with higher weights have priority
@@ -138,18 +160,14 @@ class Equipment:
         if max_resistance:
             # item useless for non builder
             if self.remaining_upgrades_res < 0:
-                return 0, list(self.stat_dict.values())[4:]
+                return 0, self.get_boosted_stats(list(self.stat_dict.values())[4:])
             remaining_upgrades = self.remaining_upgrades_res
         effective_stats = self.get_post_upgrade_stats(remaining_upgrades, copy.deepcopy(weights), upgrade_accs)
-        if self.type == "Armor":
-            quality_mult = QUALITY_PROPERTIES[self.quality][0]
-            boosted_stats = effective_stats > 0
-            boosted_stats = boosted_stats * quality_mult + ~boosted_stats
-            effective_stats = effective_stats * boosted_stats
+        effective_stats = self.get_boosted_stats(effective_stats)
         score = np.sum(effective_stats * np.array(weights))
         if sum(weights) == 0:
             return score
-        return (score / sum(weights)), [math.ceil(x) for x in effective_stats]
+        return (score / sum(weights)), effective_stats
 
     def get_post_upgrade_stats(self, remaining_upgrades, weights, upgrade_accs):
         """
@@ -172,7 +190,10 @@ class Equipment:
             if effective_stats[best_stat] == 0:
                 weights[best_stat] = -float_info.max
                 continue
-            quality_cap = QUALITY_PROPERTIES[self.quality][1]
+            if self.type == "Accessory":
+                quality_cap = 500 if self.quality > 14 else 360
+            else:
+                quality_cap = QUALITY_PROPERTIES[self.quality][1]
             if self.type == "Familiar" and self.quality == 0:
                 if "Diamond" in self.desc_string:
                     quality_cap = 800

src/equipment_handler.py
@@ -165,6 +165,14 @@ class EquipmentHandler:
                 all_equips, all_stats, all_scores = self.optimize_for_slots(
                     CHAR_SLOTS[self.target_classes[target]], weights, target, cannot_steal, protected,
                     all_equips, all_stats, all_scores, upgrade_accs=upgrade_accs)
+            else:
+                for i, equip in enumerate(self.all_equipment):
+                    if equip.owner == target and equip.type in ["Weapon", "Familiar"]:
+                        for j in range(len(all_equips)):
+                            all_equips[j].append(i)
+                            cur_score, cur_stats = equip.get_weighted_score(weights, upgrade_accs)
+                            all_stats[j] += cur_stats
+                            all_scores[j] += cur_score
         if target in self.print_targets:
             self.print_optimization_results(all_equips, all_stats, all_scores, weights, target, print_all, upgrade_accs)
         return all_equips[-1], all_scores[-1]
@@ -268,13 +276,11 @@ class EquipmentHandler:
         delim = "=" * 60
         header = list(STAT_OFFSET_DICT.keys())[4:]
         start_index = 0
-        is_dps_target = False
-        if max(weights[:4]) > 0:
-            is_dps_target = True
+        is_dps_target = True if max(weights[:4]) > 0 else False
         if not print_all:
             start_index = len(all_scores) - 1
         for i in range(start_index, len(all_scores)):
-            equip, stat, score = all_equips[i], all_stats[i], all_scores[i]
+            equip, stats, score = all_equips[i], all_stats[i], all_scores[i]
             score = math.ceil(score)
             target_str = target
             if not self.raw_output:
@@ -284,44 +290,42 @@ class EquipmentHandler:
             print(f"Rank {len(all_scores) - i} ({self.all_equipment[equip[0]].material})" + ":\n" +
                   self.generate_table(equip).__str__())
             p_table = PrettyTable(header)
-            stat = list(stat)
+            stats = list(stats)
             if not self.raw_output:
-                for j, s in enumerate(stat):
+                for j, s in enumerate(stats):
                     if weights[j] > 0:
-                        stat[j] = Style.BRIGHT + str(s) + Style.RESET_ALL
+                        stats[j] = Style.BRIGHT + str(s) + Style.RESET_ALL
                     else:
-                        stat[j] = Style.DIM + str(s) + Style.RESET_ALL
-            p_table.add_row(list(stat))
+                        stats[j] = Style.DIM + str(s) + Style.RESET_ALL
+            p_table.add_row(list(stats))
             print(f"Stats with score {score}:\n" + p_table.__str__())
-            if target != "":
-                owner_stats = np.zeros(len(weights))
-                owner_score = 0
-                for e in self.all_equipment:
-                    if e.owner == target and not \
-                            (is_dps_target and (e.type == "Weapon" or e.type == "Familiar")) and not \
-                            (self.armor_only and e.type != "Armor"):
-                        item_score, item_stats = e.get_weighted_score(weights, upgrade_accs)
-                        owner_stats += np.array(item_stats)
-                        owner_score += item_score
-                owner_score = math.ceil(owner_score)
-                stat_diffs = [int(e) for e in list(np.array(all_stats[-1]) - owner_stats)]
-                if not self.raw_output:
-                    for i in range(len(stat_diffs)):
-                        if weights[i] > 0:
-                            stat_diffs[i] = pos_neg_color(stat_diffs[i]) + str(stat_diffs[i]) + \
-                                            Style.RESET_ALL
-                p_table = PrettyTable(header)
-                p_table.add_row(stat_diffs)
-                print(f"Stat changes with score delta " +
-                      (pos_neg_color(score - owner_score) if not self.raw_output else '') +
-                      str(score - owner_score) +
-                      (Style.RESET_ALL if not self.raw_output else '') + ":\n" +
-                      p_table.__str__())
-                upgrade_cost = sum([self.all_equipment[e].get_upgrade_costs() for e in equip])
-                upgrade_cost_no_accs = sum([self.all_equipment[e].get_upgrade_costs()
-                                            if self.all_equipment[e].type != "Accessory" else 0 for e in equip])
-                print(f"Estimated upgrade cost (without accessories): {np.round(upgrade_cost / 1e9, 2)}B" +
-                      f" ({np.round(upgrade_cost_no_accs / 1e9, 2)}B)")
+            owner_stats = np.zeros(len(weights))
+            owner_score = 0
+            for e in self.all_equipment:
+                if e.owner == target and not \
+                        (self.armor_only and e.type != "Armor"):
+                    item_score, item_stats = e.get_weighted_score(weights, upgrade_accs)
+                    owner_stats += np.array(item_stats)
+                    owner_score += item_score
+            owner_score = math.ceil(owner_score)
+            stat_diffs = [int(e) for e in list(np.array(all_stats[-1]) - owner_stats)]
+            if not self.raw_output:
+                for j in range(len(stat_diffs)):
+                    if weights[j] > 0:
+                        stat_diffs[j] = pos_neg_color(stat_diffs[j]) + str(stat_diffs[j]) + \
+                                        Style.RESET_ALL
+            p_table = PrettyTable(header)
+            p_table.add_row(stat_diffs)
+            print(f"Stat changes with score delta " +
+                  (pos_neg_color(score - owner_score) if not self.raw_output else '') +
+                  str(score - owner_score) +
+                  (Style.RESET_ALL if not self.raw_output else '') + ":\n" +
+                  p_table.__str__())
+            upgrade_cost = sum([self.all_equipment[e].get_upgrade_costs() for e in equip])
+            upgrade_cost_no_accs = sum([self.all_equipment[e].get_upgrade_costs()
+                                        if self.all_equipment[e].type != "Accessory" else 0 for e in equip])
+            print(f"Estimated upgrade cost (without accessories): {np.round(upgrade_cost / 1e9, 2)}B" +
+                  f" ({np.round(upgrade_cost_no_accs / 1e9, 2)}B)")
         print(delim)
 
     def generate_table(self, indices, sort=False):